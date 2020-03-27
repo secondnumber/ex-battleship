dd4e02ed2cd34683e619e126fda547eddc5b4f1d
Yulya Vorontsova <secondnumber01@gmail.com>
2020-03-27 10:13:46 +0300

    feat(product): start making game

--- config/script.js
+++ config/script.js
@@ -1,26 +1,29 @@
-const playGame = () => {
-  let position1 = Math.floor(Math.random() * 5);
-  let position2 = position1 + 1;
-  let position3 = position2 + 1;
-  let shot = prompt('Выстрел: от 0 до 6', 100);
-  while (shot < 0 || shot > 6) {
-    shot = prompt('Выстрел: от 0 до 6', 100);
-  }
-  let count = 0;
-  console.log(position1);
-  console.log(position2);
-  console.log(position3);
-  if (
-    shot === position1 ||
-    shot === position2 ||
-    shot === position3
-  ) {
-    count += 1;
-    console.log('Yes');
-    console.log(count);
+const position1 = Math.floor(Math.random() * 5);
+const position2 = position1 + 1;
+const position3 = position2 + 1;
+let guess;
+let hits = 0;
+let guesses = 0;
+let isShunk = false;
+while (isShunk === false) {
+  guess = prompt('Введите число от 0 до 6');
+  if (guess < 0 || guess > 6) {
+    alert('Число должно быть от 0 до 6');
   } else {
-    console.log('No');
+    guesses += 1;
+    if (
+      guess === position1 ||
+      guess === position2 ||
+      guess === position3
+    ) {
+      alert('Попал!');
+      hits += 1;
+      if (hits === 3) {
+        isShunk = true;
+        alert('Корабль потоплен!');
+      }
+    } else {
+      alert('Промахнулся');
+    }
   }
-};
-
-playGame();
+}
