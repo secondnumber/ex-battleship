2f716f1d776bc879f911bb82b136b5a634506b70
Yulya Vorontsova <secondnumber@Yulyas-MacBook-Air.local>
2020-06-02 20:37:20 +0300

    fix(product): fix problem with for loop

--- config/battleship.js
+++ config/battleship.js
@@ -1,3 +1,18 @@
+const view = {
+  displayMessage(message) {
+    const messageArea = document.querySelector('.messageArea');
+    messageArea.innerHTML = message;
+  },
+  displayHit(location) {
+    const cell = document.getElementById(location);
+    cell.setAttribute('class', 'hit');
+  },
+  displayMiss(location) {
+    const cell = document.getElementById(location);
+    cell.setAttribute('class', 'miss');
+  },
+};
+
 const model = {
   boardSize: 7,
   numShips: 3,
